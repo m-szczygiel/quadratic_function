b8f523a489d219c6d989e0100f5d7c512b97f361
m-szczygiel <73465411+m-szczygiel@users.noreply.github.com>
2020-10-26 09:54:38 +0100

    Add files via upload

---
+++ quadratic_function.py
@@ -0,0 +1,22 @@
+import math
+
+
+a = int(input("a="))
+b = int(input("b="))
+c = int(input("c="))
+delta = (b*b) - 4 * a * c
+if delta > 0:
+    x1 = (-b-math.sqrt(delta))/2*a
+    x2 = (-b+math.sqrt(delta))/2*a      
+    print (delta)
+    print (x1)
+    print (x2)
+elif delta == 0:
+    x = -b/2*a
+    print (x)
+else:
+    print ("Brak miejsc zerowych")
+
+restart
+
+
